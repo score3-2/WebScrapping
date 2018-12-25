281a4275c6a6dbb2fa6249b88eea27b5d73d4453
Abel C <abelomar@icloud.com>
2018-12-24 17:58:34 -0800

    Initial Commit

---
+++ WebScrapping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebScrapping
+//
+//  Created by Abel C on 12/24/18.
+//  Copyright © 2018 Abel Caramazana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
