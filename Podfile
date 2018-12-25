9e0f29f38b5fd9ba4c080480826761c71ac3bd33
Abel C <abelomar@icloud.com>
2018-12-24 18:49:54 -0800

    Scrapping / alamofire

---
+++ Podfile
@@ -0,0 +1,14 @@
+project 'WebScrapping.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '12.0'
+
+target 'WebScrapping' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WebScrapping
+  pod 'Alamofire'
+  pod 'Kanna'
+
+end
--- WebScrapping/ViewController.swift
+++ WebScrapping/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
 class ViewController: UIViewController {
-
+    
+    // 1 Create a constant to hold the web url
+    let urlName = "https://colorhunt.co/palette/117534"
+    
+    // MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrapeURL()
     }
 
+    /* 1 Create a func to scrapp the constant url (string version of the url).
+     request property to request and hold the url.
+     then we tell the method to print on a string if there is any response
+     */
+ 
+    func scrapeURL() {
+        Alamofire.request(urlName).responseString { response in
+            print(response.result.isSuccess)
+            print(response.result.value)
+        }
+    }
 
 }
 
